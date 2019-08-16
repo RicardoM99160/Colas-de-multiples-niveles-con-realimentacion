f7e69346206905d7795ce7c1827cbfd4d8f48c89
RicardoM99160 <36250736+RicardoM99160@users.noreply.github.com>
2019-08-16 17:51:57 -0600

    colas

--- Algoritmo/Algoritmo/Form1.cs
+++ Algoritmo/Algoritmo/Form1.cs
@@ -14,30 +14,16 @@ namespace Algoritmo
     {
         public Form1()
         {
-            Random Aleatorio = new Random();
-            //Creación de proceso A
-            int prioridad1 = Aleatorio.Next(0, 5);
-            int tiempo1 = Aleatorio.Next(1, 30);
-            Proceso procesoA = new Proceso(prioridad1, tiempo1, false);
-            //Creación de proceso B
-            int prioridad2 = Aleatorio.Next(0, 5);
-            int tiempo2 = Aleatorio.Next(1, 30);
-            Proceso procesoB = new Proceso(prioridad2, tiempo2, false);
-            //Creación de proceso C
-            int prioridad3 = Aleatorio.Next(0, 5);
-            int tiempo3 = Aleatorio.Next(1, 30);
-            Proceso procesoC = new Proceso(prioridad3, tiempo3, false);
-            //Creación de proceso D
-            int prioridad4 = Aleatorio.Next(0, 5);
-            int tiempo4 = Aleatorio.Next(1, 30);
-            Proceso procesoD = new Proceso(prioridad4, tiempo4, false);
-            //Creación de proceso E
-            int prioridad5 = Aleatorio.Next(0, 5);
-            int tiempo5 = Aleatorio.Next(1, 30);
-            Proceso procesoE = new Proceso(prioridad5, tiempo5, false);
-
-
             InitializeComponent();
+            Proceso procesoA, procesoB, procesoC, procesoD, procesoE;
+
+            //Creacion de colas
+            Queue<Proceso> Cola0 = new Queue<Proceso>();
+            Queue<Proceso> Cola1 = new Queue<Proceso>();
+            Queue<Proceso> Cola2 = new Queue<Proceso>();
+            Queue<Proceso> Cola3 = new Queue<Proceso>();
+            Queue<Proceso> Cola4 = new Queue<Proceso>();
+            Queue<Proceso> Cola5 = new Queue<Proceso>();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -45,4 +31,5 @@ namespace Algoritmo
 
         }
     }
+
 }
