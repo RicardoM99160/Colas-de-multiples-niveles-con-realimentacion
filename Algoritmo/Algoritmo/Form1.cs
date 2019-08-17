1dbfc36b518b94332d2ba5ce600409c430371760
RicardoM99160 <36250736+RicardoM99160@users.noreply.github.com>
2019-08-16 18:27:52 -0600

    culo

--- Algoritmo/Algoritmo/Form1.cs
+++ Algoritmo/Algoritmo/Form1.cs
@@ -14,6 +14,7 @@ namespace Algoritmo
     {
         public Form1()
         {
+            //Creación de procesos
             Proceso[] procesos = new Proceso[5];
             Random Aleatorio = new Random();
             for (int i = 0; i < procesos.Length; i++)
@@ -22,10 +23,7 @@ namespace Algoritmo
                 int tiempo = Aleatorio.Next(1, 30);
                 procesos[i] = new Proceso(prioridad, tiempo, true);
             }
-            
 
-            InitializeComponent();
- 
             //Creacion de colas
             Queue<Proceso> Cola0 = new Queue<Proceso>();
             Queue<Proceso> Cola1 = new Queue<Proceso>();
@@ -34,136 +32,8 @@ namespace Algoritmo
             Queue<Proceso> Cola4 = new Queue<Proceso>();
             Queue<Proceso> Cola5 = new Queue<Proceso>();
 
-            //Clasificación de procesos en sus respectivas colas
-            //Clasificación proceso A:
-            if (procesoA.P==0)
-            {
-                Cola0.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 1)
-            {
-                Cola1.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 2)
-            {
-                Cola2.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 3)
-            {
-                Cola3.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 4)
-            {
-                Cola4.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 5)
-            {
-                Cola5.Enqueue(procesoA);
-            }
+            InitializeComponent();
 
-            //Clasificación proceso B:
-            if (procesoB.P == 0)
-            {
-                Cola0.Enqueue(procesoB);
-            }
-            else if (procesoB.P == 1)
-            {
-                Cola1.Enqueue(procesoB);
-            }
-            else if (procesoB.P == 2)
-            {
-                Cola2.Enqueue(procesoB);
-            }
-            else if (procesoB.P == 3)
-            {
-                Cola3.Enqueue(procesoB);
-            }
-            else if (procesoB.P == 4)
-            {
-                Cola4.Enqueue(procesoB);
-            }
-            else if (procesoB.P == 5)
-            {
-                Cola5.Enqueue(procesoB);
-            }
-
-            //Clasificación proceso C:
-            if (procesoA.P == 0)
-            {
-                Cola0.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 1)
-            {
-                Cola1.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 2)
-            {
-                Cola2.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 3)
-            {
-                Cola3.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 4)
-            {
-                Cola4.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 5)
-            {
-                Cola5.Enqueue(procesoA);
-            }
-
-            //Clasificación proceso A:
-            if (procesoA.P == 0)
-            {
-                Cola0.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 1)
-            {
-                Cola1.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 2)
-            {
-                Cola2.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 3)
-            {
-                Cola3.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 4)
-            {
-                Cola4.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 5)
-            {
-                Cola5.Enqueue(procesoA);
-            }
-
-            //Clasificación proceso A:
-            if (procesoA.P == 0)
-            {
-                Cola0.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 1)
-            {
-                Cola1.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 2)
-            {
-                Cola2.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 3)
-            {
-                Cola3.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 4)
-            {
-                Cola4.Enqueue(procesoA);
-            }
-            else if (procesoA.P == 5)
-            {
-                Cola5.Enqueue(procesoA);
-            }
 
 
         }
