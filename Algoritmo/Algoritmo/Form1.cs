12f04219f11e3a6cad5101e14267ea22bd554e5a
RicardoM99160 <36250736+RicardoM99160@users.noreply.github.com>
2019-08-16 18:51:02 -0600

    ricardo no importa

--- Algoritmo/Algoritmo/Form1.cs
+++ Algoritmo/Algoritmo/Form1.cs
@@ -12,10 +12,18 @@ namespace Algoritmo
 {
     public partial class Form1 : Form
     {
+        //Creación de procesos
+        Proceso[] procesos = new Proceso[5];
+        Queue<Proceso> Cola0;
+        Queue<Proceso> Cola1;
+        Queue<Proceso> Cola2;
+        Queue<Proceso> Cola3;
+        Queue<Proceso> Cola4;
+        Queue<Proceso> Cola5;
+
         public Form1()
         {
             //Creación de procesos
-            Proceso[] procesos = new Proceso[5];
             Random Aleatorio = new Random();
             for (int i = 0; i < procesos.Length; i++)
             {
@@ -25,12 +33,12 @@ namespace Algoritmo
             }
 
             //Creacion de colas
-            Queue<Proceso> Cola0 = new Queue<Proceso>();
-            Queue<Proceso> Cola1 = new Queue<Proceso>();
-            Queue<Proceso> Cola2 = new Queue<Proceso>();
-            Queue<Proceso> Cola3 = new Queue<Proceso>();
-            Queue<Proceso> Cola4 = new Queue<Proceso>();
-            Queue<Proceso> Cola5 = new Queue<Proceso>();
+            Cola0 = new Queue<Proceso>();
+            Cola1 = new Queue<Proceso>();
+            Cola2 = new Queue<Proceso>();
+            Cola3 = new Queue<Proceso>();
+            Cola4 = new Queue<Proceso>();
+            Cola5 = new Queue<Proceso>();
 
             InitializeComponent();
 
@@ -43,6 +51,37 @@ namespace Algoritmo
 
         }
 
+        private Proceso[] AsignacionCola(Proceso[] prcs)
+        {
+            foreach (Proceso proceso in prcs)
+            {
+                if (proceso.P == 0)
+                {
+                    Cola0.Enqueue(proceso);
+                }
+                else if (proceso.P == 1)
+                {
+                    Cola1.Enqueue(proceso);
+                }
+                else if (proceso.P == 2)
+                {
+                    Cola2.Enqueue(proceso);
+                }
+                else if (proceso.P == 3)
+                {
+                    Cola3.Enqueue(proceso);
+                }
+                else if (proceso.P == 4)
+                {
+                    Cola4.Enqueue(proceso);
+                }
+                else if (proceso.P == 5)
+                {
+                    Cola5.Enqueue(proceso);
+                }
+            }
+            return prcs;
+        }
 
     }
 
